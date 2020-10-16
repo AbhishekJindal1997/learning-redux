49134927eafcd017ae86849f79c0db596d16756d
Abhishek Jindal <Abhishek.Jindal63@gmail.com>
2020-10-16 03:50:47 -0400

    Created Older Method Folder.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './App.css';
+//import store from "./store/configureStore";
+import store from "./OlderMethods/customStore";
+import * as actions from "./OlderMethods/bugs"
+
 
 function App() {
+
+  store.subscribe(()=>{
+    console.log("STORE CHANGED");
+  })
+
+  store.dispatch(actions.bugAdded("BUG 1"));
+  store.dispatch(actions.bugAdded("BUG 2"));
+  store.dispatch(actions.bugAdded("BUG 3"));
+  store.dispatch(actions.bugResolved(1));
+
+  
+ 
+
+
+
   return (
     <div className="App">
     <h1>HELLO WORLD</h1>
@@ -10,3 +29,4 @@ function App() {
 }
 
 export default App;
+
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,52 +1,27 @@
-// Action Types
-const BUG_ADDED = 'bugAdded';
-const BUG_REMOVED = 'bugRemoved';
-const BUG_RESOLVED = 'bugResolved';
+import { createAction, createReducer } from "@reduxjs/toolkit";
+
+
 
 // Action Creators
-export const bugAdded = description => ({
-    type:BUG_ADDED,
-    payload:{
-        description
-    }
-});
-
-export const bugRemoved = id = ({
-    type:BUG_REMOVED,
-    payload:{
-        id
-    }
-});
-
-export const bugResolved = id = ({
-    type:BUG_RESOLVED,
-    payload:{
-        id
-    }
-})
+export const bugAdded = createAction("bugAdded");
+export const bugRemoved = createAction("bugRemoved");
+export const bugResolved = createAction("bugResolved");
 
 //Reducer
 let lastId= 0;
+export default createReducer([],{
+  
+    [bugAdded.type]:(bugs, action) => {
+        bugs.push({
+            id:++lastId,
+            description:action.payload.description,
+            resolved:false
+        });
+    },
 
-export default function reducer(state =[] , action){
-
-    switch(action.type){
-
-        case BUG_ADDED:
-            return[
-                ...state,
-                {
-                    id:++lastId,
-                    description:payload.description,
-                    resolved:false
-                }
-            ];
-         
-        case BUG_REMOVED:
-            return state.filter(bug=>bug.id !== action.id);
-         
-        default:
-            return state;   
+    [bugResolved.type]:(bugs,action) =>{
+        const index = bugs.findIndex(bug => bug.id === action.payload.id);
+        bugs[index].resolved = true;
     }
-}
+})
 
