65f336101ac6b0d2b8c7b86c6744af53e0ee2001
Abhishek Jindal <Abhishek.Jindal63@gmail.com>
2020-10-21 04:04:51 -0400

    Testing using jest

---
+++ src/math.spec.js
@@ -0,0 +1,18 @@
+import {isEven} from "./math";
+
+describe('isEven', () => {
+
+    it("should return true if given an even number", () => {
+
+        //Function under test known as System Under test (SUT)
+        const result = isEven(2);
+        expect(result).toEqual(true);
+    });
+    
+    it("should return false if given an odd number", ()=>{
+        const result = isEven(1);
+        expect(result).toEqual(false);
+    
+    }); 
+})
+
